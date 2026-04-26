643c4e9fc5032e1e21de6249b6c0e0d9b9d93882
Vinay Tiwari <132244480+Z3ro-Kun@users.noreply.github.com>
2026-04-26 18:57:23 +0530

    Create code.py

---
+++ 1077_B/code.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def main():
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    count = 0
+    i = 1
+
+    while i < n - 1:
+        if a[i - 1] == 1 and a[i] == 0 and a[i + 1] == 1:
+            count += 1
+            i += 3
+        else:
+            i += 1
+
+    print(count)
+
+if __name__ == "__main__":
+    main()
