3249a1c9f7febcd994452527a715bf9640518353
Vinay Tiwari <132244480+Z3ro-Kun@users.noreply.github.com>
2026-04-26 18:32:39 +0530

    Implement sliding window to maximize book count

---
+++ 279_B/code.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+
+
+
+
+def main():
+    n, t = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    l = 0
+    current_sum = 0
+    max_books = 0
+
+    for r in range(n):
+        current_sum += a[r]
+
+        while current_sum > t:
+            current_sum -= a[l]
+            l += 1
+
+        max_books = max(max_books, r - l + 1)
+
+    print(max_books)
+
+
+if __name__ == "__main__":
+    main()
